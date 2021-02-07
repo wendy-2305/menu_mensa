32b7341e315bec62481be9c62def4f1ca59fd809
49179 <49179@LAPTOP-3LO8AMIO>
2021-02-07 20:33:45 +0100

    Photos et application erweiterung

--- Menu_Mensa-222222/src/main/java/entit/ten/MenuMensa222222Application.java
+++ Menu_Mensa-222222/src/main/java/entit/ten/MenuMensa222222Application.java
@@ -20,10 +20,16 @@ public class MenuMensa222222Application implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		menuRepository.save(new Menu(1L,"Pasta","image1", 2.5));
-		menuRepository.save(new Menu(2L,"macaroni","image2", 3.0));
-		menuRepository.save(new Menu(3L,"pizza","image3",4.00));
-		menuRepository.save(new Menu(4L,"burger","image4",4.00));
+		menuRepository.save(new Menu(1L,"Pasta","Pasta", 2.5));
+		menuRepository.save(new Menu(2L,"maccaroni","Mccaroni and cheese", 3.0));
+		menuRepository.save(new Menu(3L,"pizza","Pizza",4.00));
+		menuRepository.save(new Menu(4L,"burger","Bürger",6.00));
+		menuRepository.save(new Menu(5L,"Fisch","Fisch",8.00));
+		menuRepository.save(new Menu(6L,"Schwein","schwein",4.00));
+		menuRepository.save(new Menu(7L,"gulasch-vom-kalb-mit-spaetzle","Rindf",9.70));
+		menuRepository.save(new Menu(8L,"salat","salat",7.00));
+		menuRepository.save(new Menu(9L,"Vegaan burger","Bürger",12.00));
+		menuRepository.save(new Menu(10L,"Reis","tomatenreis",10.00));
 		
 		
 		for (Menu menu : menuRepository.findAll()) {
--- Menu_Mensa-222222/src/main/java/entit/ten/Menu_Service.java
+++ Menu_Mensa-222222/src/main/java/entit/ten/Menu_Service.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 	@RequestMapping("/api")
 	public class Menu_Service {
